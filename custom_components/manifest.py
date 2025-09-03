bdd86e09332686b86a6d1d7f42e81d6b195fb377
smartfilterpro <eric@smartfilterpro.com>
2025-09-02 21:12:46 -0400

    Create manifest.py

---
+++ custom_components/manifest.py
@@ -0,0 +1,13 @@
+{
+  "domain": "smartfilterpro",
+  "name": "SmartFilterPro",
+  "version": "0.2.0",
+  "documentation": "https://smartfilterpro.com/ha",
+  "issue_tracker": "https://github.com/your-org/smartfilterpro-ha/issues",
+  "config_flow": true,
+  "requirements": [],
+  "codeowners": ["@your-github"],
+  "iot_class": "cloud_push",
+  "integration_type": "service",
+  "loggers": ["custom_components.smartfilterpro"]
+}
